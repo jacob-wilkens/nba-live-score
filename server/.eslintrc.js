07bab1ee4d2041d48296abdf424806357d668352
jacob-wilkens <jacob.wilkens20@gmail.com>
2023-02-06 03:14:43 +0000

    Adding client and server

---
+++ server/.eslintrc.js
@@ -0,0 +1,23 @@
+/** @type {import('eslint').Linter.Config} */
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+    es2022: true,
+  },
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:unicorn/recommended', 'plugin:sonarjs/recommended', 'plugin:promise/recommended', 'prettier'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint', 'unicorn', 'autofix'],
+  rules: {
+    'unicorn/filename-case': ['error', { cases: { camelCase: true, pascalCase: true } }],
+    'autofix/no-unused-vars': ['error', { argsIgnorePattern: '^_', ignoreRestSiblings: true, destructuredArrayIgnorePattern: '^_' }],
+    '@typescript-eslint/consistent-type-imports': ['error', { prefer: 'type-imports' }],
+    'no-restricted-imports': ['error', { patterns: ['../'] }],
+    'unicorn/prefer-node-protocol': 'off',
+    'unicorn/prefer-module': 'off',
+  },
+};
--- src/App.tsx
+++
@@ -1,9 +0,0 @@
-function App() {
-  return (
-    <div id='main'>
-      <h1>Hello World</h1>
-    </div>
-  );
-}
-
-export default App;
